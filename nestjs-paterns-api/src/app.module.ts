65a5463fe9a5ff191170163bf647d85bf7ae2114
NascimentoTalesDev <111396464+NascimentoTalesDev@users.noreply.github.com>
2024-06-22 18:45:18 -0400

    chore: add prisma mod&serv

--- nestjs-paterns-api/src/app.module.ts
+++ nestjs-paterns-api/src/app.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { EventsModule } from './events/events.module';
+import { PrismaModule } from './prisma/prisma.module';
 import { SpotsModule } from './spots/spots.module';
 import { EventsModule } from './events/events.module';
 
 @Module({
-  imports: [EventsModule, SpotsModule],
+  imports: [EventsModule, SpotsModule, PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
